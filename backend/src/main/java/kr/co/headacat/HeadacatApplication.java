2c73257ad91927c2bb9989331f137815c30f2754
kukkoro-admin <bellline0403@gmail.com>
2024-12-11 20:57:49 +0900

    initialization.

---
+++ backend/src/main/java/kr/co/headacat/HeadacatApplication.java
@@ -0,0 +1,13 @@
+package kr.co.headacat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeadacatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HeadacatApplication.class, args);
+    }
+
+}
---
+++ backend/src/test/java/kr/co/headacat/HeadacatApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.headacat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadacatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
